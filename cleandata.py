962115cd279d84c16f216330367e642b79379eec
Brandon der Blätter <intcreator@users.noreply.github.com>
2017-12-28 18:46:23 -0800

    Prepare data for machine learning

--- cleandata.py
+++ cleandata.py
@@ -1,8 +1,18 @@
 import os
+import csv
 
-filename = "Raw_Essay_Data.csv"
+os.makedirs("30_min_essays")
+os.makedirs("10_min_essays")
 
-with open(filename) as f:
-    student_data = f.readlines()
+essay_filename = "Raw_Essay_Data.csv"
+essay_data = csv.reader(open(essay_filename, newline='', encoding='latin-1'), quotechar='"')
 
-print(student_data)
+for row_num, row in enumerate(essay_data):
+    if not row_num == 0:
+        new30_filename = f'30-{row[1]}-{float(row[4]):.2f}'
+        with open(os.path.join("30_min_essays", new30_filename),"w+") as f:
+            f.write(row[3])
+
+        new10_filename = f'10-{row[1]}-{float(row[4]):.2f}'
+        with open(os.path.join("10_min_essays", new10_filename),"w+") as f:
+            f.write(row[2])
